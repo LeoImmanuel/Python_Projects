fb9c1d42a53e24d0564b3397fb0d30094b85c689
LEO IMMANUEL <leoimmanuelj@gmail.com>
2024-12-15 16:30:07 +0530

    Add files via upload

---
+++ Game/madlibs.py
@@ -0,0 +1,7 @@
+# String concatenation 
+adj = str(input("Adjective: "))
+verb = str(input("Verb: "))
+madlib = f"Computer prgrammming is {adj}! It makes me \
+    so exicted all the time because I love to {verb}"
+
+print(madlib)
