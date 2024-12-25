72c6dbf6b99aa616dd6ca1c80587191fd19d50f0
LEO IMMANUEL <leoimmanuelj@gmail.com>
2024-12-25 10:21:05 +0530

    Add files via upload
    patterns

---
+++ Python_Programs/Pattern6.py
@@ -0,0 +1,11 @@
+#55555
+#4444
+#333
+#22
+#1
+
+n = 5
+for i in range(n):
+    for j in range(n-i):
+        print(n-i,end="")
+    print()
---
+++ Python_Programs/Pattern8.py
@@ -0,0 +1,12 @@
+#12345
+#12345
+#12345
+#12345
+#12345
+
+n = 5
+for i in range(n):
+    for j in range(n):
+        print(j+1, end="")
+    print()
+    
